272df4d877ead87f87fae2f33707790ac19ffaf1
valerioperria <valerio.perria@gmail.com>
2024-01-29 19:05:07 +0100

    feat. technology_model and connection with project_model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -23,4 +23,8 @@ class Project extends Model
     public function type() {
         return $this->belongsTo(Type::class);
     }
+
+    public function technologies() {
+        return $this->belongsToMany(Technology::class);
+    }
 }
